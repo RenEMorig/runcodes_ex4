2a497e382f349f15cbed3cb21bfab87e5076252a
RenEMorig <renmori17@usp.br>
2025-03-27 22:18:40 -0300

    Add files via upload

---
+++ runcodes_ex4.c
@@ -0,0 +1,24 @@
+
+
+
+#include <stdio.h>
+
+int comp2num(int num1, int num2) {
+
+/*     return num1 >= num2 ? num1 : num2; */
+
+    if(num1 >= num2) {
+        return num1;
+    } else {
+        return num2;
+    }
+}
+
+int main() {
+    int a, b, c, maior;
+
+    scanf("%d\n%d\n%d", &a, &b, &c);
+    
+    printf("%d", comp2num(a,comp2num(b,c)));
+
+}
